b69966e512e7e04e6ff03ff1de19bce45a3f23a1
vov <ovcharenkovv@gmail.com>
2015-04-17 18:54:30 +0300

    add index genre test stub

---
+++ tests/GenreTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class GenreTest extends TestCase {
+
+    /**
+     * A test for genres endpoint
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+//        $response = $this->call('GET', '/genres');
+//
+//        $this->assertResponseOk();
+//
+//        $data = json_decode($response->getContent(),true);
+//        $this->assertArrayHasKey('name', $data[0]);
+//        $this->assertArrayHasKey('bg', $data[0]);
+//
+//        $this->assertArrayNotHasKey('undefined', $data[0]);
+
+    }
+
+}
