31a8ac88350c0d90ae869874a93a19efa4bb78a4
Ovcharenko Volodymyr <ovcharenkovv@gmail.com>
2015-04-21 00:15:25 +0300

    seed jazz genre

---
+++ database/seeds/GenreTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GenreTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('genres')->delete();
+        DB::insert(
+            'insert into genres
+            (sh_id, name, sh_name, radios_amount, bg) values (?, ?, ?, ?, ?)',
+            [777, 'Jazz','Jazz Sh',50,'/img/jazz.jpg']
+        );
+
+    }
+
+}
