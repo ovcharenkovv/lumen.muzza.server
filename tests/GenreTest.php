ae86f664e57947eaaadbe98ccdff2207feb1676d
Ovcharenko Volodymyr <ovcharenkovv@gmail.com>
2015-04-21 00:26:48 +0300

    add genre schema test

--- tests/GenreTest.php
+++ tests/GenreTest.php
@@ -9,15 +9,19 @@ class GenreTest extends TestCase {
      */
     public function testIndex()
     {
-//        $response = $this->call('GET', '/genres');
-//
-//        $this->assertResponseOk();
-//
-//        $data = json_decode($response->getContent(),true);
-//        $this->assertArrayHasKey('name', $data[0]);
-//        $this->assertArrayHasKey('bg', $data[0]);
-//
-//        $this->assertArrayNotHasKey('undefined', $data[0]);
+        $response = $this->call('GET', '/genres');
+
+        $this->assertResponseOk();
+
+        $data = json_decode($response->getContent(),true);
+
+        $this->assertArrayHasKey('sh_id', $data[0]);
+        $this->assertArrayHasKey('name', $data[0]);
+        $this->assertArrayHasKey('sh_name', $data[0]);
+        $this->assertArrayHasKey('radios_amount', $data[0]);
+        $this->assertArrayHasKey('bg', $data[0]);
+
+        $this->assertArrayNotHasKey('undefined', $data[0]);
 
     }
 
