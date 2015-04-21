dcff23c2b06fc0110032b311b5c1523a72f14747
vov <ovcharenkovv@gmail.com>
2015-04-21 19:17:06 +0300

    add genre api endpoint test

--- tests/GenreTest.php
+++ tests/GenreTest.php
@@ -9,19 +9,29 @@ class GenreTest extends TestCase {
      */
     public function testIndex()
     {
-//        $response = $this->call('GET', '/genres');
-//
-//        $this->assertResponseOk();
-//
-//        $data = json_decode($response->getContent(),true);
-//
-//        $this->assertArrayHasKey('sh_id', $data[0]);
-//        $this->assertArrayHasKey('name', $data[0]);
-//        $this->assertArrayHasKey('sh_name', $data[0]);
-//        $this->assertArrayHasKey('radios_amount', $data[0]);
-//        $this->assertArrayHasKey('bg', $data[0]);
-//
-//        $this->assertArrayNotHasKey('undefined', $data[0]);
+        $response = $this->call('GET', '/genres');
+
+        $this->assertResponseOk();
+
+        $data = json_decode($response->getContent(),true);
+
+        $this->assertArrayHasKey('sh_id', $data[0]);
+        $this->assertArrayHasKey('name', $data[0]);
+        $this->assertArrayHasKey('sh_name', $data[0]);
+        $this->assertArrayHasKey('radios_amount', $data[0]);
+        $this->assertArrayHasKey('bg', $data[0]);
+
+        $this->assertArrayNotHasKey('undefined', $data[0]);
+
+        $this->assertEquals(777, $data[0]['sh_id']);
+        $this->assertEquals('Jazz', $data[0]['name']);
+        $this->assertEquals('Jazz Sh', $data[0]['sh_name']);
+        $this->assertEquals(50, $data[0]['radios_amount']);
+        $this->assertEquals('/img/jazz.jpg', $data[0]['bg']);
+
+        $this->assertNotEquals('undefined', $data[0]['name']);
+
+
 
     }
 
