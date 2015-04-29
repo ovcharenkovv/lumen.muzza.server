b12c3c714ec8b73e9a5209d6758dff8c171da96d
vov <ovcharenkovv@gmail.com>
2015-04-29 10:24:13 +0300

    add one more seed row in the genres table

--- database/seeds/GenreTableSeeder.php
+++ database/seeds/GenreTableSeeder.php
@@ -7,11 +7,19 @@ class GenreTableSeeder extends Seeder {
     public function run()
     {
         DB::table('genres')->delete();
+
         DB::insert(
             'insert into genres
             (sh_id, name, sh_name, radios_amount, bg) values (?, ?, ?, ?, ?)',
             [777, 'Jazz','Jazz Sh',50,'/img/jazz.jpg']
         );
+
+        DB::insert(
+            'insert into genres
+            (sh_id, name, sh_name, radios_amount, bg) values (?, ?, ?, ?, ?)',
+            [800, 'Chill','Chill Sh',0,'/img/chill.jpg']
+        );
+
 
     }
 
