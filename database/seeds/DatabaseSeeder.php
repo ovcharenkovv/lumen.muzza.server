885be05f5a8cd6f959c8f529edc15afdf5296b6c
vov <ovcharenkovv@gmail.com>
2015-05-05 17:52:41 +0300

    revert default seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,6 +9,17 @@ class DatabaseSeeder extends Seeder {
     public function run()
     {
         Model::unguard();
+
+        $this->call('GenreTableSeeder');
+        $this->command->info('Genre table seeded!');
+
+        $this->call('RadioTableSeeder');
+        $this->command->info('Radio table seeded!');
+
+        $this->call('RadioTrackTableSeeder');
+        $this->command->info('RadioTrack table seeded!');
+
+
     }
 
 }
