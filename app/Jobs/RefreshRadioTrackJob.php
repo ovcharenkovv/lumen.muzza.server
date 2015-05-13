2f90fa7b9b4ff524cf5bb16fd19024531d681cee
vov <ovcharenkovv@gmail.com>
2015-05-13 09:45:43 +0300

    add doc block for job

--- app/Jobs/RefreshRadioTrackJob.php
+++ app/Jobs/RefreshRadioTrackJob.php
@@ -2,16 +2,29 @@
 
 use App\Services\RadioTrackManager;
 
+/**
+ * Class RefreshRadioTrackJob
+ * @package App\Jobs
+ */
 class RefreshRadioTrackJob extends Job
 {
+    /**
+     * @var
+     */
     protected $radioId;
 
 
+    /**
+     * @param $radioId
+     */
     function __construct($radioId)
     {
         $this->radioId = $radioId;
     }
 
+    /**
+     * @param RadioTrackManager $radioTrackManager
+     */
     public function handle(RadioTrackManager $radioTrackManager)
     {
         $radioTrackManager->refreshTracks($this->radioId);
