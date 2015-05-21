62a20ffac3d96c6f3b4cc56d9cf60c6f4ce875bb
vov <ovcharenkovv@gmail.com>
2015-05-21 11:12:07 +0300

    decouple test class and fix hard code id

--- tests/Services/RadioTrackManagerSaveTrackMethodTest.php
+++ tests/Services/RadioTrackManagerSaveTrackMethodTest.php
@@ -1,57 +1,11 @@
 <?php
-//use App\Repositories\RadioRepository as RadioRepo;
+
 use App\Repositories\RadioTrackRepository as RadioTrackRepo;
 use App\Services\RadioTrackManager;
 
-//use App\Services\Shoutcast\ShoutcastClient as Client;
-
-class RadioTrackManagerTest extends TestCase
+class RadioTrackManagerSaveTrackMethodTest extends TestCase
 {
 
-    /**
-     * Test for track validation
-     */
-    public function testIsTrackValid()
-    {
-        $clientMock = $this->getMock('App\Services\Shoutcast\ShoutcastClient',[],[],'',false);
-        $radioTrackRepoMock = $this->getMock('App\Repositories\RadioTrackRepository');
-        $radioRepoMock = $this->getMock('App\Repositories\RadioRepository');
-
-        $trackManager = new RadioTrackManager(
-            $clientMock,
-            $radioTrackRepoMock,
-            $radioRepoMock
-        );
-
-
-
-        $method = $this->getMethod('App\Services\RadioTrackManager', 'isTrackValid');
-
-        $this->assertTrue(
-            $method->invoke($trackManager, "Track Name - Artist")
-        );
-        $this->assertTrue(
-            $method->invoke($trackManager, "One - Two - Tree")
-        );
-
-
-        $this->assertFalse(
-            $method->invoke($trackManager, "Test Test")
-        );
-        $this->assertFalse(
-            $method->invoke($trackManager, null)
-        );
-        $this->assertFalse(
-            $method->invoke($trackManager, "")
-        );
-        $this->assertFalse(
-            $method->invoke($trackManager, " ")
-        );
-        $this->assertFalse(
-            $method->invoke($trackManager, "  \n\t")
-        );
-
-    }
 
     /**
      * Track add to db
@@ -63,7 +17,7 @@ class RadioTrackManagerTest extends TestCase
         $radioId = $this->generateRandomId();
 
         DB::insert('INSERT INTO genres (id) VALUES (?)', [$genreId]);
-        DB::insert('INSERT INTO radios (id, genre_id) VALUES (?, ?)', [$radioId, 1]);
+        DB::insert('INSERT INTO radios (id, genre_id) VALUES (?, ?)', [$radioId, $genreId]);
 
         $clientMock = $this->getMock('App\Services\Shoutcast\ShoutcastClient', [], [], '', false);
         $radioTrackRepoMock = $this->getMock('App\Repositories\RadioTrackRepository');
@@ -98,7 +52,7 @@ class RadioTrackManagerTest extends TestCase
         $radioId = $this->generateRandomId();
 
         DB::insert('INSERT INTO genres (id) VALUES (?)', [$genreId]);
-        DB::insert('INSERT INTO radios (id, genre_id) VALUES (?, ?)', [$radioId, 1]);
+        DB::insert('INSERT INTO radios (id, genre_id) VALUES (?, ?)', [$radioId, $genreId]);
 
         $clientMock = $this->getMock('App\Services\Shoutcast\ShoutcastClient', [], [], '', false);
         $radioTrackRepoMock = $this->getMock('App\Repositories\RadioTrackRepository');
